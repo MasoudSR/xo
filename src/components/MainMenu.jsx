7661cbc2535a328c0724a76a4a3652e8ab1cdefd
Masoud S.R <amir.sepehr72@gmail.com>
2024-09-20 14:08:46 +0330

    ui buttons improved

--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -42,19 +42,19 @@ function MainMenu({ setGameMode, sides, setSides, setConnection, setPlayerNumber
                     </p>
                 </div>
                 <div className={`flex items-center flex-col gap-2 transition-all duration-300 ${playMode !== "ai" ? "scale-100 h-44" : "scale-0 h-1"}`}>
-                    <button className={`bg-blue-500 rounded-full shadow-md shadow-blue-300 transition-all duration-300 text-white ${playMode === "friend" ? "h-0 w-0" : " h-8 w-32"}`} onClick={() => setPlayMode("ai")}>With AI</button>
-                    <div className={`transition-all duration-500 flex flex-col items-center bg-white drop-shadow-md overflow-hidden  ${playMode === "friend" ? "w-64 rounded-xl" : "w-32 rounded-[20px]"}`}>
-                        <button className={`w-full h-8 ${playMode === "friend" && "cursor-default"}`} onClick={() => setPlayMode("friend")}>With a friend</button>
-                        <div className={`flex transition-all duration-300 overflow-hidden origin-top ${playMode === "friend" ? "h-8" : "h-0"}`}>
-                            <button className='h-8 w-32 border-r border-t' onClick={() => setOnlineMenu(true)}>Online</button>
-                            <button className='h-8 w-32 border-t' onClick={() => changeMode("local")}>Local</button>
+                    <button className={`bg-blue-500 rounded-full shadow-md shadow-blue-300 transition-all duration-300 text-white ${playMode === "friend" ? "h-0 w-0" : " h-12 w-40"}`} onClick={() => setPlayMode("ai")}>With AI</button>
+                    <div className={`transition-all duration-500 flex flex-col items-center bg-white drop-shadow-md overflow-hidden  ${playMode === "friend" ? "w-64 rounded-xl" : "w-40 rounded-[26px]"}`}>
+                        <button className={`w-full h-12 ${playMode === "friend" && "cursor-default"}`} onClick={() => setPlayMode("friend")}>With a friend</button>
+                        <div className={`flex transition-all duration-300 overflow-hidden origin-top ${playMode === "friend" ? "h-10" : "h-0"}`}>
+                            <button className='h-10 w-32 border-r border-t' onClick={() => setOnlineMenu(true)}>Online</button>
+                            <button className='h-10 w-32 border-t' onClick={() => changeMode("local")}>Local</button>
                         </div>
                     </div>
                 </div>
                 <button className='drop-shadow-md rounded-full flex bg-slate-200 overflow-hidden'>
-                    <button className={`h-8 transition-all duration-300 bg-white ${playMode !== "" ? "w-20 border-r" : "w-0 border-none"}`} onClick={() => setPlayMode("")}>Back</button>
+                    <button className={`h-10 transition-all duration-300 bg-white ${playMode !== "" ? "w-20 border-r" : "w-0 border-none"}`} onClick={() => setPlayMode("")}>Back</button>
                     {/* <button className={`h-8 transition-all duration-300 flex justify-center items-center bg-white ${playMode === "" ? "w-8" : "w-0"}`} onClick={() => setPlayMode("")}><IoMdSettings size={20} color='#4281f8' /></button> */}
-                    <button className={`h-8 transition-all duration-300  bg-white ${playMode === "ai" ? "w-32" : "w-0"}`} onClick={() => setGameMode("ai")}>Continue</button>
+                    <button className={`h-10 transition-all duration-300  bg-white ${playMode === "ai" ? "w-32" : "w-0"}`} onClick={() => setGameMode("ai")}>Continue</button>
                 </button>
             </div>
             {onlineMenu && <Modal ModalContent={<OnlineMultiplayer setConnection={setConnection} changeMode={changeMode} setPlayerNumber={setPlayerNumber} setOnlineMenu={setOnlineMenu} />} />
--- src/components/OnlineMultiplayer.jsx
+++ src/components/OnlineMultiplayer.jsx
@@ -45,14 +45,11 @@ const OnlineMultiplayer = ({ setConnection, changeMode, setPlayerNumber, setOnli
     setPeer(newPeer);
 
     newPeer.on('open', (id) => {
-      console.log("Peer ID:", id);
       setPeerId(id);
     });
 
     newPeer.on('connection', (conn) => {
-      console.log("Connection received:", conn);
       conn.on('data', (data) => {
-        console.log("Data received on host:", data);
         if (data === 'join') {
           changeMode('online');
           setConnection(conn)
@@ -73,15 +70,12 @@ const OnlineMultiplayer = ({ setConnection, changeMode, setPlayerNumber, setOnli
 
   const handleJoin = () => {
     const conn = peer.connect(hostId);
-    console.log("Connecting to host:", hostId);
 
     conn.on('open', () => {
-      console.log("Connection opened:", conn);
       conn.send('join');
     });
 
     conn.on('data', (data) => {
-      console.log("Data received from host:", data);
       if (data === 'online') {
         changeMode('online');
         setConnection(conn)
@@ -100,12 +94,12 @@ const OnlineMultiplayer = ({ setConnection, changeMode, setPlayerNumber, setOnli
   return (
     <div className={`bg-white/60 rounded-3xl flex flex-col drop-shadow-lg p-5 transition-all duration-500 ${mode === "normal" ? "gap-4" : "gap-0"} ${isClosed && "animate-jump-out"}`}>
 
-      <div className={`transition-all duration-500 flex flex-col gap-2 overflow-hidden ${mode === "client" ? "max-h-0" : "max-h-72"}`}>
+      <div className={`transition-all duration-500 flex flex-col gap-2 overflow-hidden ${mode === "client" ? "max-h-0" : "max-h-80"}`}>
         <div
           className={`transition-all duration-500 bg-blue-600 rounded-xl overflow-hidden ${showHost ? "max-h-56" : "max-h-16"
             }`}>
           <button
-            className={` p-5 transition-all duration-500 z-20 w-full shadow-md ${showHost ? "bg-blue-300 text-black" : "bg-blue-500 text-white"
+            className={`p-5 transition-all duration-500 z-20 w-full shadow-md ${showHost ? "bg-blue-300 text-black" : "bg-blue-500 text-white"
               } `}
             onClick={() => {
               setShowHost(true)
@@ -114,10 +108,11 @@ const OnlineMultiplayer = ({ setConnection, changeMode, setPlayerNumber, setOnli
             Host
           </button>
           <div className='flex flex-col p-4 gap-3'>
+            {peerId ? 
             <div className="flex text-white justify-between items-center">
               <div>
                 <p className="text-[11px] sm:text-xs">Send your id to your friend</p>
-                <p className="text-[15px] sm:text-sm w-72 mr-2">{peerId ? peerId : "Loading your id"}</p>
+                <p className="text-[15px] sm:text-sm w-full mr-2">{peerId ? peerId : "Loading your id"}</p>
               </div>
               <button
                 className={`transition-all duration-300 flex flex-col border justify-center items-center rounded-lg h-10 w-10 sm:w-12 sm:h-12  border-gray-200 ${peerId ? "scale-100" : "scale-0"}`}
@@ -126,6 +121,14 @@ const OnlineMultiplayer = ({ setConnection, changeMode, setPlayerNumber, setOnli
                 <span className="text-[8px]">Copy</span>
               </button>
             </div>
+                  : 
+                  <div className='flex flex-col justify-center items-center p-4'>
+            <div className='rounded-full border-4 w-3 border-slate-300 border-t-lime-500 p-4 animate-spin'></div>
+            <p className='text-white'>
+              Preparing...
+            </p>
+          </div>}
+
             <button className='bg-blue-500 px-5 py-3 rounded-xl text-white' onClick={() => backHandler()}>Back</button>
           </div>
         </div>
@@ -134,7 +137,7 @@ const OnlineMultiplayer = ({ setConnection, changeMode, setPlayerNumber, setOnli
 
       <div className={`transition-all duration-500 flex flex-col gap-2 overflow-hidden ${mode === "host" ? "max-h-0" : "max-h-72"}`}>
         <div
-          className={`transition-all duration-500 bg-blue-600 rounded-xl overflow-hidden ${showClient ? "max-h-56" : "max-h-16"
+          className={`transition-all duration-500 bg-blue-600 rounded-xl overflow-hidden ${showClient ? "max-h-80" : "max-h-16"
             }`}>
           <button
             className={`p-5 transition-all duration-500 z-20 w-full shadow-md ${showClient ? "bg-blue-300 text-black" : "bg-blue-500 text-white"
@@ -145,13 +148,21 @@ const OnlineMultiplayer = ({ setConnection, changeMode, setPlayerNumber, setOnli
             }}>
             Client
           </button>
-          <div className="p-4 flex flex-col gap-2 text-white justify-between items-center">
-            <input className='border rounded-lg px-2 py-1 w-full text-black' type="text" placeholder="Enter Host ID" value={hostId} onChange={(e) => setHostId(e.target.value)} />
-            <div className='flex gap-2 w-full'>
-              <button className='bg-blue-500 px-5 rounded-xl' onClick={() => backHandler()}>Back</button>
-              <button className='px-5 py-2 bg-green-600 rounded-xl w-full text-white' onClick={handleJoin}>Join</button>
+          {peerId ?
+            <div className="p-4 flex flex-col gap-2 text-white justify-between items-center">
+              <input className='border rounded-lg px-2 py-1 w-full text-black' type="text" placeholder="Enter Host ID" value={hostId} onChange={(e) => setHostId(e.target.value)} />
+              <div className='flex gap-2 w-full'>
+                <button className='bg-blue-500 px-5 rounded-xl' onClick={() => backHandler()}>Back</button>
+                <button className='px-5 py-2 bg-green-600 rounded-xl w-full text-white' onClick={handleJoin}>Join</button>
+              </div>
             </div>
-          </div>
+            : <div className='flex flex-col justify-center items-center p-4'>
+            <div className='rounded-full border-4 w-3 border-slate-300 border-t-lime-500 p-4 animate-spin'></div>
+            <p className='text-white'>
+              Preparing...
+            </p>
+          </div>}
+          
         </div>
       </div>
       <div className={`transition-all duration-300 overflow-hidden ${mode === "normal" ? "max-h-28" : "max-h-0"}`}>
